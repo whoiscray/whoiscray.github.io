5e3b58d33232ac0d0a7a68bae5f1cbdede3b6a8c
Conor C <whoiscray@mailfence.com>
2021-11-08 15:06:14 +0000

    updated scroll to top button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,17 @@
-var link = document.getElementById("back-to-top");
-  var amountScrolled = 250;
-
-  window.addEventListener('scroll', function(e) {
-      if ( window.pageYOffset > amountScrolled ) {
-          link.classList.add('show');
-      } else {
-          link.className = 'back-to-top';
-      }
-  });
-
-  link.addEventListener('click', function(e) {
-      e.preventDefault();
-
-      var distance = 0 - window.pageYOffset;
-      var increments = distance/(500/16);
-      function animateScroll() {
-          window.scrollBy(0, increments);
-          if (window.pageYOffset <= document.body.offsetTop) {
-              clearInterval(runAnimation);
-          }
-      };
-      // Loop the animation function
-      var runAnimation = setInterval(animateScroll, 16);
-  });
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 300;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
